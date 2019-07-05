fbd3d6ac896d445aa0270216408f5d2e8daef1ae
Borislav Arangelov <b.arangelov@mail.bg>
2019-07-05 12:50:54 +0300

    Second commit

--- src/main/java/PizzaShop/Controller/Controller.java
+++ src/main/java/PizzaShop/Controller/Controller.java
@@ -5,7 +5,6 @@ import PizzaShop.AccountRepository.AccountRepository;
 import PizzaShop.Entities.Product;
 import PizzaShop.ProductRepository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +14,7 @@ import java.util.List;
 public class Controller {
     private final AccountRepository accountRepository;
     private final ProductRepository productRepository;
-
+    private Account logged = null;
     @Autowired
     public Controller(AccountRepository accountRepository, ProductRepository productRepository) {
         this.accountRepository = accountRepository;
@@ -25,6 +24,7 @@ public class Controller {
 
     @GetMapping("/")
     public ModelAndView index(ModelAndView modelAndView) {
+        logged = null;
         modelAndView.setViewName("base-layout");
         modelAndView.addObject("view", "views/index");
         return modelAndView;
@@ -32,20 +32,24 @@ public class Controller {
 
     @PostMapping("/")
     public String login(Account account){
-        if (checkLogin(account)) return "redirect:/";
+        if (checkAccount(account)) return "redirect:/";
         List<Account> accounts = this.accountRepository.findAll();
         Account acc = accounts.stream()
                 .filter(account1 -> account.getUsername().equals(account1.getUsername()))
                 .findAny()
                 .orElse(null);
         if(acc.getUsername().equals(account.getUsername()) && acc.getPassword().equals(account.getPassword())){
+            logged = acc;
+            if(acc.isAdmin()) {
+                return "redirect:/pizzasAdmin";
+            }
             return "redirect:/pizzas";
         }
             return "redirect:/";
     }
 
-    private boolean checkLogin(Account account) {
-        if(account.getUsername() == null || account.getPassword() == null){
+    private boolean checkAccount(Account account) {
+        if(account.getUsername() == null || account.getPassword() == null || account.getUsername().trim().isEmpty() || account.getPassword().trim().isEmpty()){
             return true;
         }
         return false;
@@ -60,16 +64,18 @@ public class Controller {
 
     @PostMapping("/register")
     public String create(Account account){
+        if (checkAccount(account)) return "redirect:/register";
         accountRepository.saveAndFlush(account);
         return "redirect:/";
     }
 
     @GetMapping("/pizzas")
-    public ModelAndView homepage(ModelAndView modelAndView){
+    public ModelAndView pizzas(ModelAndView modelAndView){
         List<Product> products = this.productRepository.findAllByType("pizza");
         modelAndView.setViewName("base-layout");
         modelAndView.addObject("view", "views/pizzas");
         modelAndView.addObject("products", products);
+        modelAndView.addObject("account", logged);
         return modelAndView;
     }
 
@@ -79,6 +85,29 @@ public class Controller {
        modelAndView.setViewName("base-layout");
        modelAndView.addObject("view", "views/drinks");
        modelAndView.addObject("products", products);
+       modelAndView.addObject("account", logged);
        return modelAndView;
    }
+
+   @GetMapping("/pizzasAdmin")
+   public ModelAndView pizzasAdmin(ModelAndView modelAndView){
+       List<Product> products = this.productRepository.findAllByType("pizza");
+       modelAndView.setViewName("base-layout");
+       modelAndView.addObject("view", "views/pizzasAdmin");
+       modelAndView.addObject("products", products);
+       modelAndView.addObject("account", logged);
+       return modelAndView;
+   }
+
+    @GetMapping("/drinksAdmin")
+    public ModelAndView drinksAdmin(ModelAndView modelAndView){
+        List<Product> products = this.productRepository.findAllByType("drink");
+        modelAndView.setViewName("base-layout");
+        modelAndView.addObject("view", "views/drinksAdmin");
+        modelAndView.addObject("products", products);
+        modelAndView.addObject("account", logged);
+        return modelAndView;
+    }
+
+
 }
